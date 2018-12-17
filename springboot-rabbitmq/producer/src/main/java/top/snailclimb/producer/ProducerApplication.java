a83769d8477210a380a791e172dc5f513a931e4c
Snailclimb <koushuangbwcx@163.com>
2018-12-17 15:21:46 +0800

    SpringBoot 整合 RabbitMQ

---
+++ springboot-rabbitmq/producer/src/main/java/top/snailclimb/producer/ProducerApplication.java
@@ -0,0 +1,14 @@
+package top.snailclimb.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
+
---
+++ springboot-rabbitmq/producer/src/test/java/top/snailclimb/producer/ProducerApplicationTests.java
@@ -0,0 +1,17 @@
+package top.snailclimb.producer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProducerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
