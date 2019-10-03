6ef400ca51ff7293ffb2d772af9d956110f8197c
Kou Shuang <koushuangbwcx@163.com>
2019-10-03 09:44:52 +0800

    Spring Boot 版 Hello World 代码上传

---
+++ start/hello-world/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
---
+++ start/hello-world/src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class HelloWorldController {
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
