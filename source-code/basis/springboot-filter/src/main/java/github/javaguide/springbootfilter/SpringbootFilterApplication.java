4bbb0177f9a55ca2e1d5e6c117f8fc6a06cecc30
Kou Shuang <koushuangbwcx@163.com>
2019-10-22 18:23:22 +0800

    spring boot 整合过滤器项目创建

---
+++ source-code/basis/springboot-filter/src/main/java/github/javaguide/springbootfilter/SpringbootFilterApplication.java
@@ -0,0 +1,13 @@
+package github.javaguide.springbootfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFilterApplication.class, args);
+    }
+
+}
---
+++ source-code/basis/springboot-filter/src/test/java/github/javaguide/springbootfilter/SpringbootFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package github.javaguide.springbootfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
